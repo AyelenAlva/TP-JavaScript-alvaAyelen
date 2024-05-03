bf3f5ff10a5bffbfec060fbc19993883060f5a9a
IPF . <ayelenalva506@gmail.com>
2024-05-03 10:19:49 -0300

    ejercicio par e impar terminado

---
+++ assets/parImpar.js
@@ -0,0 +1,6 @@
+let numero= prompt ("ingrese un numero")
+if (numero % 2 == 0){
+    console.log ("El numero: " + numero + " es par");
+}else{
+    console.log ("El numero: " + numero + " es impar" );
+}
