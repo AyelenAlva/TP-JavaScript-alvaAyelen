0c9973dace97d8628c1790579219606fea79e6b3
IPF . <ayelenalva506@gmail.com>
2024-05-06 11:21:19 -0300

    ejercicio numeros ascendentes terminado

--- assets/numAscendente.js
+++ assets/numAscendente.js
@@ -1,5 +1,33 @@
-let num1= prompt("ingrese el primer numero")
-let num2= prompt ("ingrese el segundo numero")
+let num2 = prompt ("ingrese el primer numero")
+let num1 = prompt ("ingrese el segundo numero")
 let num3 = prompt ("ingrese el tercer numero")
+console.log("Números de menor a mayor:");
 
-let numeros = (num1, num2, num3)
+if (num1 <= num2 && num1 <= num3) {
+    console.log(num1);
+    if (num2 <= num3) {
+        console.log(num2);
+        console.log(num3);
+    } else {
+        console.log(num3);
+        console.log(num2);
+    }
+} else if (num2 <= num1 && num2 <= num3) {
+    console.log(num2);
+    if (num1 <= num3) {
+        console.log(num1);
+        console.log(num3);
+    } else {
+        console.log(num3);
+        console.log(num1);
+    }
+} else {
+    console.log(num3);
+    if (num1 <= num2) {
+        console.log(num1);
+        console.log(num2);
+    } else {
+        console.log(num2);
+        console.log(num1);
+    }
+}
